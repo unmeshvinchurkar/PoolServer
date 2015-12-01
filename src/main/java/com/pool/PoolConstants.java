522fde3d64553f5f185cf2d62c8311a2e1ef5044
unmeshvinchurkar <unmeshv02@gmail.com>
2015-12-01 20:46:22 +0530

    checking in constants.

--- src/main/java/com/pool/PoolConstants.java
+++ src/main/java/com/pool/PoolConstants.java
@@ -4,4 +4,23 @@ public class PoolConstants {
 
 	public static String USER_SESSION_ATTR = "USER";
 
+	public static final Long REQUEST_JOIN_POOL_REQUEST_id = 1L;
+
+	public static final Long NOTI_POOL_DISSOLVED_ID = 1L;
+	public static final Long NOTI_TRAVELLER_REMOVED_ID = 2L;
+	public static final Long NOTI_TRAVELLER_OPTED_OUT_ID = 3L;
+	public static final Long NOTI_POOL_HOLIDAY_ID = 4L;
+	public static final Long NOTI_TRAVELLER_HOLIDAY_ID = 5L;
+	public static final Long NOTI_JOIN_REQUEST_ACCEPTED_ID = 6L;
+	public static final Long NOTI_JOIN_REQUEST_REJECTED_ID = 7L;
+
+	public static final String REQUEST_JOIN_POOL_REQUEST_NAME = "JOIN_POOL_REQUEST";
+	public static final String NOTI_POOL_DISSOLVED_TYPE_NAME = "POOL_DISSOLVED";
+	public static final String NOTI_TRAVELLER_REMOVED_TYPE_NAME = "TRAVELLER_REMOVED";
+	public static final String NOTI_TRAVELLER_OPTED_OUT_TYPE_NAME = "TRAVELLER_OPTED_OUT";
+	public static final String NOTI_POOL_HOLIDAY_TYPE_NAME = "POOL_HOLIDAY";
+	public static final String NOTI_TRAVELLER_HOLIDAY_TYPE_NAME = "TRAVELLER_HOLIDAY";
+	public static final String NOTI_JOIN_REQUEST_ACCEPTED_TYPE_NAME = "JOIN_REQUEST_ACCEPTED";
+	public static final String NOTI_JOIN_REQUEST_REJECTED_TYPE_NAME = "JOIN_REQUEST_REJECTED";
+
 }
